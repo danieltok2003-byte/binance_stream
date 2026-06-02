912a667bb0587d9f0e2e55761929c7c07bd26a13
danieltok2003-byte <danieltok2003@gmail.com>
2026-06-02 15:47:40 +0800

    up basic concept to stream

--- producer.py
+++ producer.py
@@ -1,6 +1,14 @@
 from confluent_kafka import Producer
 import uuid
 import json
+import asyncio
+import logging
+from dotenv import load_dotenv
+from binance_common.configuration import ConfigurationWebSocketStreams
+from binance_common.constants import SPOT_WS_STREAMS_PROD_URL
+from binance_sdk_spot.spot import Spot
+
+load_dotenv()
 
 producer_config = {
     'bootstrap.servers' : 'localhost:9092' # define 1 broker as starting point to discover full list of servers in cluster
@@ -8,29 +16,43 @@ producer_config = {
 
 producer = Producer(producer_config)
 
-order = {
-    'order_id': str(uuid.uuid4()),
-    'user': 'will',
-    'item': 'chicken burger',
-    'quantity': 1
-}
+logging.basicConfig(level=logging.INFO)
 
-value = json.dumps(order).encode('utf-8')
-
-def delivery_report(err, msg):
-    if err:
-        print(f'Delivery failed: {err}')
-    else:
-        print(f"Delivery succeeded: {msg.value().decode('utf-8')}")
-        print(f"Delivered to topic {msg.topic()}, partition: {msg.partition()} at offset: {msg.offset()}")
-
-producer.produce(
-    topic='orders', 
-    value=value,
-    callback=delivery_report # call after deliver
+configuration_ws_streams = ConfigurationWebSocketStreams(
+    stream_url=SPOT_WS_STREAMS_PROD_URL,
 )
 
-producer.flush() # group and send in batch, not 1 by 1 (if fail, before terminate sends unsent - always call before end)
+client = Spot(config_ws_streams=configuration_ws_streams)
+
+def handle_data(data):
+    logging.info(data)
+    producer.produce(
+        topic='orders', 
+        value=f"{data}",
+    )
+async def agg_trade():
+    connection = None
+    try:
+        connection = await client.websocket_streams.create_connection()
+
+        stream = await connection.agg_trade(
+            symbol="bnbusdt",
+        )
+        stream.on("message", handle_data)
+        
+
+        await asyncio.sleep(5)
+        await stream.unsubscribe()
+    except Exception as e:
+        logging.error(f"agg_trade() error: {e}")
+    finally:
+        if connection:
+            
+            await connection.close_connection(close_session=True)
+            producer.flush() # group and send in batch, not 1 by 1 (if fail, before terminate sends unsent - always call before end
+
+if __name__ == "__main__":
+    asyncio.run(agg_trade())
 
 
 
